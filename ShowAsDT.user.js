fd17df33c6bc1fa38d2f9aa9f22bc90daff43a46
DarkStarFTW <sunnyzuo@protonmail.com>
2019-01-12 22:32:07 -0700

    Update ShowAsDT.user.js

--- ShowAsDT.user.js
+++ ShowAsDT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     DT Redirect
 // @description Redirect Bitcointalk URLs to Show as DT
-// @version  0.2.1
+// @version  0.2.2
 // @author DarkStar_
 // @include       https://bitcointalk.org/*
 // @grant       none
@@ -20,15 +20,16 @@
           a.href += ";dt";
       }
   });
-  document.querySelectorAll("a[href^='/index.php']").forEach(a => {
-      if (! /;dt/.test (a)
-          && a != "index.php"
-          && ! /action=search/.test (a)
-          && ! /more\.php/.test (a)
-          && ! /donate.html/.test (a)
-          && ! /#new/.test (a)
-          && ! /#msg/.test (a)){
-          a.href += ";dt";
+  if (window.location.host === "bitcointalk.org") {
+      document.querySelectorAll("a[href^='index.php']").forEach(a => {
+          if (! /;dt/.test (a)
+              && a != "index.php"
+              && ! /action=search/.test (a)
+              && ! /more\.php/.test (a)
+              && ! /donate.html/.test (a)
+              && ! /#new/.test (a)
+              && ! /#msg/.test (a)){
+              a.href += ";dt";
       }
-  });
+  })};
 })();

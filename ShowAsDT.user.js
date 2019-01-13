14003e31eba1692e5da69cbc8779a9b17e898182
DarkStarFTW <sunnyzuo@protonmail.com>
2019-01-13 10:44:13 -0700

    Update ShowAsDT.user.js

--- ShowAsDT.user.js
+++ ShowAsDT.user.js
@@ -1,35 +1,47 @@
 // ==UserScript==
 // @name     DT Redirect
 // @description Redirect Bitcointalk URLs to Show as DT
-// @version  0.2.2
+// @version  0.2.3
 // @author DarkStar_
 // @include       https://bitcointalk.org/*
 // @grant       none
 // ==/UserScript==
 
 (() => {
-  document.querySelectorAll("a[href^='https://bitcointalk.org/']").forEach(a => {
-      if (! /;dt/.test (a)
-          && a != "https://bitcointalk.org/index.php"
-          && a != "https://bitcointalk.org/"
-          && ! /action=search/.test (a)
-          && ! /more\.php/.test (a)
-          && ! /donate.html/.test (a)
-          && ! /#new/.test (a)
-          && ! /#msg/.test (a)){
-          a.href += ";dt";
-      }
-  });
-  if (window.location.host === "bitcointalk.org") {
-      document.querySelectorAll("a[href^='/index.php']").forEach(a => {
-          if (! /;dt/.test (a)
-              && a != "/index.php"
-              && ! /action=search/.test (a)
-              && ! /more\.php/.test (a)
-              && ! /donate.html/.test (a)
-              && ! /#new/.test (a)
-              && ! /#msg/.test (a)){
-              a.href += ";dt";
-      }
-  })};
+    document.querySelectorAll("a[href^='https://bitcointalk.org/']").forEach(a => {
+        if (!/;dt/.test(a) &&
+            a != "https://bitcointalk.org/index.php" &&
+            a != "https://bitcointalk.org/" &&
+            !/action=search/.test(a) &&
+            !/more\.php/.test(a) &&
+            !/donate.html/.test(a) &&
+            !/#new/.test(a) &&
+            !/#msg/.test(a)) {
+            a.href += ";dt";
+        }
+    });
+    if (window.location.host === "bitcointalk.org") {
+        document.querySelectorAll("a[href^='/index.php']").forEach(a => {
+            if (!/;dt/.test(a) &&
+                a != "/index.php" &&
+                !/action=search/.test(a) &&
+                !/more\.php/.test(a) &&
+                !/donate.html/.test(a) &&
+                !/#new/.test(a) &&
+                !/#msg/.test(a)) {
+                a.href += ";dt";
+            }
+        })
+        document.querySelectorAll("a[href^='index.php']").forEach(a => {
+            if (!/;dt/.test(a) &&
+                a != "index.php" &&
+                !/action=search/.test(a) &&
+                !/more\.php/.test(a) &&
+                !/donate.html/.test(a) &&
+                !/#new/.test(a) &&
+                !/#msg/.test(a)) {
+                a.href += ";dt";
+            }
+        })
+    };
 })();

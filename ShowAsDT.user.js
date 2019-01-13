90df603ab73384000bd78fa1d97d7c252bee82b1
DarkStarFTW <sunnyzuo@protonmail.com>
2019-01-12 22:13:52 -0700

    Update ShowAsDT.user.js

--- ShowAsDT.user.js
+++ ShowAsDT.user.js
@@ -1,23 +1,22 @@
 // ==UserScript==
 // @name     DT Redirect
 // @description Redirect Bitcointalk URLs to Show as DT
-// @version  0.1
+// @version  0.2
 // @author DarkStar_
-// @match       https://*.bitcointalk.org/*
-// @run-at      document-start
+// @include       https://bitcointalk.org/*
 // @grant       none
 // ==/UserScript==
 
-
-if (! /;dt/.test (window.location.href) 
-    && window.location.href != "https://bitcointalk.org/index.php" 
-    && window.location.href != "https://bitcointalk.org/" 
-    && ! /action=search/.test (window.location.href)
-    && ! /more\.php/.test (window.location.href)
-    && ! /donate.html/.test (window.location.href)
-    && ! /#new/.test (window.location.href)
-    && ! /#msg/.test (window.location.href)
-   ) {
-	var newURL = window.location.href + ";dt";
-	window.location.replace (newURL);
-}
+(() => {
+  document.querySelectorAll("a[href^='https://bitcointalk.org/']").forEach(a => {
+      if (! /;dt/.test (a)&& a != "https://bitcointalk.org/index.php"
+          && a != "https://bitcointalk.org/"
+          && ! /action=search/.test (a)
+          && ! /more\.php/.test (a)
+          && ! /donate.html/.test (a)
+          && ! /#new/.test (a)
+          && ! /#msg/.test (a)){
+          a.href += ";dt";
+      }
+  });
+})();
